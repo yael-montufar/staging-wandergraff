a4f6122f57fd1b472c8b4d2c7c2a8c84e1f9bf3b
Yael Montufar <yael.montufar@gmail.com>
2025-11-21 11:21:21 -0600

    fix local dev server for builder

---
+++ builder.config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'ef3383dbe09945f8b10d38edb6e136a3',
+  devCommand: 'npm run dev',
+  devServerUrl: 'http://localhost:5173',
+  port: 48752,
+  cors: {
+    origin: ['http://localhost:5173', 'https://builder.io'],
+    credentials: true
+  }
+};
